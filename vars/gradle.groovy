a1d4322fa3535b6d5277423046cc2ecad0de0ab6
Cristian Martinez <cristian.martinez.perez@gmail.com>
2020-12-20 16:41:37 -0300

    modificacion ejecuccio.groovy paso 6 modifica gradle.groovy con parametros

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,40 +1,76 @@
 
-def call(){
+def call(String stages){
+
+                       println 'Recibidos parametros ' + stages
+					   
+					   Jobs = Stages.split(';')
+					   
+					   def bEjecutarBT = false
+					   def bEjecutarSonar = false;
+					   def bEjecutarRun = false;
+					   def bEjecutarRest = false;
+					   def bEjecutarNexus = false;
+					   
+					   
+					   for( String job : Jobs )
+						{
+						   if (job == 'Build & Test') bEjecutarBT = true;
+						   if (job == 'Sonar') bEjecutarSonar = true;
+						   if (job == 'Run') bEjecutarRun = true;
+						   if (job == 'Rest') bEjecutarRest = true;
+						   if (job == 'Nexus') bEjecutarNexus = true;						   
+						}
   
 				       stage('Build & Test')
 					   {
-					      env.TAREA = env.STAGE_NAME
+					     if (bEjecutarBT)
+						 {
+						  env.TAREA = env.STAGE_NAME
 					      bat "gradle clean build"  
+						 }
 
 					   }
 				       stage('Sonar')
 					   {
-							env.TAREA = env.STAGE_NAME
+					      if (bEjecutarSonar)
+						  {
+						  	env.TAREA = env.STAGE_NAME
 					   		def scannerHome = tool 'sonar-scanner';
 							
 							withSonarQubeEnv('SonarQube') { 
 							  bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
 
 							}
+						  }
+
 					   }	
 				       stage('Run')
 					   {
-					      env.TAREA = env.STAGE_NAME
+					     if (bEjecutarRun)
+						 {
+						  env.TAREA = env.STAGE_NAME
 						  bat "nohup start gradle bootRun &"
 						  sleep 20
-
+						 }
 					   }	
 				       stage('Rest')
 					   {
-					       env.TAREA = env.STAGE_NAME
+					      if (bEjecutarRest)
+						  {
+						    env.TAREA = env.STAGE_NAME
 					       bat 'curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing'
+						  }
 					   }					   
 				   	   stage('Nexus')
 					   {
-					        env.TAREA = env.STAGE_NAME
+					      if (bEjecutarNexus)
+						  {
+						  	env.TAREA = env.STAGE_NAME
 					        nexusPublisher nexusInstanceId: 'Nexus',
 							nexusRepositoryId: 'test-nexus',
 							packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\cmartinez\\Documents\\personal\\devops\\Unidad 3\\tarea11\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+
+						  }
 
 					   }
 				   
