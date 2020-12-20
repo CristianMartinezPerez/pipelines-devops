0188015cb0a985d55913eb7354ebeb0fd82da8fe
Cristian Martinez <cristian.martinez.perez@gmail.com>
2020-12-20 16:46:07 -0300

    modificacion ejecuccion.groovy paso 6 modifica ejecucion.groovy con parametros

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -36,7 +36,7 @@ def call(){
 								  {
 								  	   // println "Ejecucion :" + params.herramienta
 									   if (params.herramienta == 'gradle') 
-									       gradle.call()
+									       gradle.call(validStages)
 										else
 										   maven.call()
 								  }
