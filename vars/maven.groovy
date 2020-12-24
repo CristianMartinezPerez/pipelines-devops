1029e4f80a981d45cb71c6ee1d75486730bf8c32
Cristian Martinez <cristian.martinez.perez@gmail.com>
2020-12-24 15:00:11 -0300

    modificacion se agrega workspace paso 8 fix 4

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -80,7 +80,7 @@ def call(String stages){
 			env.TAREA = env.STAGE_NAME
 			nexusPublisher nexusInstanceId: 'Nexus',
 			nexusRepositoryId: 'test-nexus',
-			packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "${env.WORKSPACE}\\ejemplo-maven\\build\\DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+			packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "${env.WORKSPACE}\\build\\DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 
 		}
 	}
